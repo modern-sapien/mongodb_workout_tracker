ae04be655353e35e43f9f70dd894de01ce6c9f97
Lexx <joncanales2@gmail.com>
2020-10-22 17:08:09 -0400

    updating server file for connection to mongo atlas & heroku.

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
+const workoutController = require("./controllers/workoutController")
 
 const PORT = process.env.PORT || 3000;
-
-const workoutController = require("./controllers/workoutController")
 
 // express middleware
 app.use(express.urlencoded({ extended: true }));
@@ -16,42 +15,42 @@ app.use(express.static("public"));
 
 // mongoose middleware
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // mongoose connected successfully 
 const connection = mongoose.connection;
-  connection.on("connected", () => {
-    console.log("Mongoose successfully connected.");
-  });
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected.");
+});
 
 // Logs if there is an error on connection
 connection.on("error", (err) => {
-    console.log("Mongoose connection error: ", err);
-    });
+  console.log("Mongoose connection error: ", err);
+});
 
-app.get("/api/config", (req,res)    =>  {   
-    res.json({
-        success: true,
-    });
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
 });
 
 app.use("/", workoutController);
 
-app.get("/exercise", function (req, res)  {
+app.get("/exercise", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/exercise.html"));
 })
 
-app.get("/stats", function (req, res)  {
+app.get("/stats", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/stats.html"));
 })
 
-app.listen(PORT, () =>  {
-    console.log(`App is running on http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 });
